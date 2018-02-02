4c9756f1376791f050696abbe6f4c83eee36ffe3
PelayoAlvarez <pelayoalvarez11@gmail.com>
2018-02-02 14:03:36 +0100

    CrearMovimiento funcionando. Falta aplicar css.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Banco from './containers/Banco/Banco';
 import { Route, Switch } from 'react-router-dom';
+import CreateMovimiento from './containers/movimientos/CreateMovimiento';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
-            <Route path="/" component={Banco} />
+            <Route path="/" component={CreateMovimiento} />
           </Switch>
       </div>
     );
---
+++ src/containers/movimientos/CreateMovimiento.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as actions from "../../store/actions";
+import { Form, Checkbox, Button } from 'semantic-ui-react';
+import axios from 'axios';
+
+class CreateMovimiento extends Component {
+
+    constructor() {
+        super();
+        this.state = {value:null, descripcion:null,
+             importe:0}
+    }
+
+    crearMovimiento = (cuenta) => {
+        axios.post('http://localhost:8080/movimiento/?cuenta='+cuenta, {importe: this.state.importe,
+            tipo: this.state.value, descripcion: this.state.descripcion, usuario: 'pepe'})
+            .then(function (response){
+                console.log(response);
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+    
+    handleChange = (e, { value }) => this.setState({ value })
+
+    actualizaImporte= (event) => {
+        this.setState({importe:event.target.value});
+    }
+
+    actualizaDescripcion= (event) => {
+        this.setState({descripcion:event.target.value});
+    }
+
+    render() {
+        return (
+            <html>
+                <header>CrearMovimiento</header>
+                <br/>
+                <div className="mov">
+                    <div class="ui segments">
+                    <div class="ui segment">
+                        <div class="ui input">
+                            <input type="text" placeholder="Importe" onChange={this.actualizaImporte} />
+                            <div class="ui basic label">€</div>
+                        </div>
+                    </div>
+                    <div class="ui segment">
+
+                    <Form>
+                        <Form.Field>
+                        Tipo: <b>{this.state.value}</b>
+                        </Form.Field>
+                        <Form.Field>
+                        <Checkbox
+                            radio
+                            label='Abono'
+                            name='checkboxRadioGroup'
+                            value='ABONO'
+                            checked={this.state.value === 'ABONO'}
+                            onChange={this.handleChange}
+                        />
+                        </Form.Field>
+                        <Form.Field>
+                        <Checkbox
+                            radio
+                            label='Cargo'
+                            name='checkboxRadioGroup'
+                            value='CARGO'
+                            checked={this.state.value === 'CARGO'}
+                            onChange={this.handleChange}
+                        />
+                        </Form.Field>
+                    </Form>
+                    
+                    </div>
+                    <div class="ui segment"> 
+                        <input type="text" placeholder="Descripcion" onChange={this.actualizaDescripcion}/>
+                    </div>
+                    </div>
+                    <div>
+                        <Button>Cancelar</Button>
+                        <Button 
+                                onClick={() => this.crearMovimiento('6182057801123304201775953')}>Guardar</Button>
+                    </div>
+                </div>
+            </html>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+      test: state.test,
+    }
+  }
+
+  const mapDispatchToProps = dispatch => {
+    return {
+      ejemplo: data => {
+        dispatch(actions.ejemploAction)
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateMovimiento);
