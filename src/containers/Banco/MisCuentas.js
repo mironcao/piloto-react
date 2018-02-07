7ce81ae9c3efb8c1757810e30fddcd75a1a70302
Miron Cao <magicadri@gmail.com>
2018-02-07 09:28:58 +0100

    Ventana MisCuentas terminada

--- src/containers/Banco/MisCuentas.js
+++ src/containers/Banco/MisCuentas.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import * as actions from "../../store/actions";
 import axios from 'axios';
 import {Button, Table} from 'semantic-ui-react';
+import {withRouter} from 'react-router-dom';
 
 class MisCuentas extends Component {
 	constructor(){
@@ -22,28 +23,32 @@ actualizarDni=(event)=>  {
 	this.setState({dni:event.target.value});
 }
 
+mostrarMovimientos=(numeroCuenta)=> {
+	this.props.listarMovimientos(numeroCuenta);
+	this.props.history.push('/misMovimientos');
+}
+
 mostrarCosas=()=> {
 	let rows=[];
 	let index=0;
 	for(let c of this.state.cuentas) {
 		index=0;
-		rows.push(<Table.Body>);
-		rows.push(<Table.Row>);
-		rows.push(<Table.Cell rowspan={c.titulares.length}>{c.numeroCuenta}</Table.Cell>);
 		for(let t of c.titulares) { 
-			if(index==0) {
-				rows.push(<Table.Cell>{t.nombre}</Table.Cell>);
-				rows.push(<Table.Cell>{t.apellidos}</Table.Cell>);
-				rows.push(</Table.Row>);
+			if(index===0) {
+				rows.push(<Table.Row>
+				<Table.Cell rowspan={c.titulares.length}>{c.numeroCuenta}</Table.Cell>
+				<Table.Cell>{t.nombre}</Table.Cell>
+				<Table.Cell>{t.apellidos}</Table.Cell>
+				<Table.Cell rowspan={c.titulares.length}><Button onClick={()=>this.mostrarMovimientos(c.numeroCuenta)}>Mostrar movimientos</Button></Table.Cell>
+				</Table.Row>)
 			} else {
-				rows.push(<Table.Row>);
-				rows.push(<Table.Cell>{t.nombre}</Table.Cell>);
-				rows.push(<Table.Cell>{t.apellidos}</Table.Cell>);
-				rows.push(</Table.Row>);
+				rows.push(<Table.Row>
+				<Table.Cell>{t.nombre}</Table.Cell>
+				<Table.Cell>{t.apellidos}</Table.Cell>
+				</Table.Row>)
 			}
 			index=index+1;
 		}
-		rows.push(</Table.Body>);
 	}
 	return rows;
 }
@@ -59,10 +64,11 @@ render(){
 				<Table.HeaderCell>Número Cuenta</Table.HeaderCell>
 				<Table.HeaderCell>Nombre</Table.HeaderCell>
 				<Table.HeaderCell>Apellidos</Table.HeaderCell>
+				<Table.HeaderCell>Movimientos</Table.HeaderCell>
 			</Table.Row>
 		</Table.Header>
 		<Table.Body>
-		{this.mostrarCosas()}
+				{this.mostrarCosas()}
 		</Table.Body>
 	</Table>
 	</div>
@@ -71,9 +77,23 @@ render(){
 
 }
 
+const mapStateToProps = state => {
+	return {
+		cuentas:state.cuentas
+	}
+}
 
+const mapDispatchToProps = dispatch => {
+	return {
+		listarMovimientos: numeroCuenta => {
+			dispatch(actions.listarMovimientos(numeroCuenta))
+		}
+	}
+}
 
-export default MisCuentas;
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(MisCuentas));
+
+/*export default MisCuentas;*/
 
 
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,15 @@
 const initialState = {
-    test: true
+    test: true,
+	numeroCuenta:""
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
+		case action.MOVIMIENTOS:
+			return {
+				...state,
+				numeroCuenta:action.numeroCuenta
+			}
         default:
         return state;
     }
