c7148ae480be9bb66333bb2adef6932b33c60910
PelayoAlvarez <pelayoalvarez11@gmail.com>
2018-02-08 09:00:05 +0100

    MisMovimientos y CrearMovimiento funcionando. Falta aplicar css.

--- src/containers/Banco/MisCuentas.js
+++ src/containers/Banco/MisCuentas.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import * as actions from "../../store/actions";
+/*import {connect} from 'react-redux';
+import * as actions from "../../store/actions";*/
 import axios from 'axios';
 
 class MisCuentas extends Component {
