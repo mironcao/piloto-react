b5be160353285a07b80a1927eacff9841e6fae5d
PelayoAlvarez <pelayoalvarez11@gmail.com>
2018-02-08 10:45:00 +0100

    Errores de mergeo

---
+++ src/components/Prueba.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react';
+
+const Prueba = (props) => {
+    return (
+        <Button>{props.nombre}</Button>
+    );
+};
+
+export default Prueba;
